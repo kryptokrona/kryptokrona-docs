1581ebddbd773fed467b6eabdbe99e5fb2b617c2
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-01-21 23:19:27 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@ set -euox pipefail
 
 git remote add origin-wiki https://$GITHUB_API_KEY@github.com/turtlecoin/turtlecoin-docs.wiki.git
 git remote add project-wiki https://$GITHUB_API_KEY@github.com/turtlecoin/turtlecoin.wiki.git
+git pull HEAD:master
 git push origin-wiki HEAD:master
 git push project-wiki HEAD:master
 
