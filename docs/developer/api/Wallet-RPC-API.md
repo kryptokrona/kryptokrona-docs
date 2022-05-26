a5414ed68eff50a35f69afb8dcd4b746c79792a5
f-r00t <fruktstav@gmail.com>
2022-05-27 00:06:04 +0200

    Forgot some files

--- docs/developer/api/Wallet-RPC-API.md
+++
@@ -1,5 +0,0 @@
----
-title: Wallet RPC API
----
-
-API Documentation is available [here](https://turtlecoin.github.io/wallet-api-docs) for wallet-api
--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euox pipefail
-
-git remote add origin-wiki https://$GITHUB_API_KEY@github.com/turtlecoin/turtlecoin-docs.wiki.git
-git remote add project-wiki https://$GITHUB_API_KEY@github.com/turtlecoin/turtlecoin.wiki.git
-git push --force origin-wiki HEAD:master
-git push --force project-wiki HEAD:master
-
-echo "Deployed to wiki"
